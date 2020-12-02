baa5652a08adfcb6c926323ae614cd83937be064
b-mp-e <47792661+b-mp-e@users.noreply.github.com>
2020-12-01 18:13:30 -0800

    Update PersonalPageTabs.js

--- frontend/src/components/PersonalPageTabs.js
+++ frontend/src/components/PersonalPageTabs.js
@@ -12,7 +12,7 @@ function PersonalPageTabs({name}) {
       <div>
         <Link to='/settings'><button className='tag-btn' id="test_personal_page_personal_button">Personal</button></Link>
         <Link to='/favorite-recipes'><button className='tag-btn' id="test_personal_page_tabs_favorite_button">Favorite</button></Link>
-        <Link to='/ingredients'><button className='tag-btn' id="test_personal_page_tabs_preferrence_button">Preferrence</button></Link>
+        <Link to='/ingredients'><button className='tag-btn' id="test_personal_page_tabs_preferrence_button">Preference</button></Link>
       </div>
       <style jsx='true'>
         {`
